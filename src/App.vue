cb67e2ef997e782ddb653d8404787ef5387a8d11
Larry Kagan <larry@superiocity.com>
2022-05-05 21:38:55 -0400

    Initial import

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <CustomTextInput
+      v-model="value"
+      v-model:lastName="lastName"
+  />
+  <br />
+  First Name: {{ value }}
+  <br />
+  Last Name: {{ lastName }}
+
+
+</template>
+
+<script setup>
+import CustomTextInput from "@/components/CustomTextInput.vue";
+import { ref } from "vue";
+
+const value = ref('');
+const lastName = ref('');
+</script>
---
+++ src/components/CustomTextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <label for="">First Name</label>
+  <input
+      type="text"
+      placeholder="Input"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+  />
+  <br />
+  <label for="">Last Name</label>
+  <input
+      type="text"
+      placeholder="Input"
+      :value="lastName"
+      @input="$emit('update:lastName', $event.target.value)"
+  />
+</template>
+
+<script setup>
+
+defineProps({
+  modelValue: String,
+  lastName: String
+});
+
+</script>
+
+<style>
+</style>
